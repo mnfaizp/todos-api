3526c7e23951dbe33a0d301777baf85fb8c41507
mnfaizp <mnfaizp@gmail.com>
2022-08-04 22:37:07 +0700

    models testing

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  # check model relationship with Item model
+  it { should belong_to(:todo) }
+  
+  # Validation Test
+  # check column availability
+  it { should validate_presence_of(:name) }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  # check model relationship with Item model
+  it { should have_many(:items).dependent(:destroy) }
+  
+  # Validation Test
+  # check column availability
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
