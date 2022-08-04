566446dbec33873665206540071c5dcfa15c01c5
mnfaizp <mnfaizp@gmail.com>
2022-08-05 05:47:37 +0700

    fix test todo

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "Todos API", type: :request do
 
         it "returns todos" do
             expect(json).not_to be_empty
-            expect(json.size).to.eq(10)
+            expect(json.size).to eq(10)
         end
 
         it "returns status code 200" do
@@ -27,7 +27,7 @@ RSpec.describe "Todos API", type: :request do
         context "when the record exists" do
             it "returns the todo" do
                 expect(json).not_to be_empty
-                expect(json[id]).to eq(todo_id)
+                expect(json['id']).to eq(todo_id)
             end
             
             it "return status code 200" do
@@ -60,8 +60,8 @@ RSpec.describe "Todos API", type: :request do
                 expect(json['title']).to eq('Learn Elm')
             end
 
-            it 'return status code 200' do
-                expect(response).to have_http_status(200)
+            it 'return status code 201' do
+                expect(response).to have_http_status(201)
             end
         end
 
