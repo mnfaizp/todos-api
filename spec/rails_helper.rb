a8184692b0e7c55900159bd21f0887b3d0cc42e4
mnfaizp <mnfaizp@gmail.com>
2022-08-04 22:36:48 +0700

    rails_helper fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactorBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
@@ -65,7 +65,7 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
-  config.infer_spec_type_from_file_location!
+  # config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
