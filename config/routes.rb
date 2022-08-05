d5117792df1b5e94019e2cb858f77f3c5595833f
mnfaizp <mnfaizp@gmail.com>
2022-08-05 07:56:32 +0700

    v2 versioning api

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  scope module: :v2, constraints: ApiVersion.new('v2') do
+    resources :todos, only: :index
+  end
+
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :todos do
       resources :items
